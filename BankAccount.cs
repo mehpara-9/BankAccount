35ccc91321ebf52112189d5f722b5f96ede4ef00
mehpara-9 <mehparafk-bb208@code.edu.az>
2025-03-25 10:45:41 +0400

    Add files via upload

---
+++ BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public class BankAccount
+    {
+        private decimal _balance;
+        public decimal Balance { get; }
+
+        public BankAccount(decimal Balance)
+        {
+            if (Balance > 0)
+            {
+                _balance = Balance;
+            }
+        }
+    
+    public string Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                return "Deposit meblegi 0-dan kicik ola bilmez";
+            }
+            _balance 
+        }
+
+
+    
+
+    
+    
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Balans ne qeder olacaq?");
+            decimal balans = Convert.ToDecimal(Console.ReadLine());
+            BankAccount bankAccount1 = new BankAccount(balans);
+            string userAnswer = "";
+            while (userAnswer != "0")
+            {
+                Console.WriteLine("HAnsi emeliyyati icra etmek isteyirsen?");
+                Console.WriteLine("1.Deposit\r\n 2.Withdraw\r\n 3.AccountInfo\r\n 4.Change OwnerName\r\n 5.TransferFunds\r\n 0.Quit Application"); userAnswer =
+                userAnswer = Console.ReadLine();
+                switch (userAnswer)
+                {
+                    case "1":
+                        Console.WriteLine("Deposit elave etmek istediyiniz meblegi qeyd edin");
+                        decimal depositAmount = Convert.ToDecimal(Console.ReadLine());
+                        bankAccount1.Deposit(depositAmount);
+                        break;
+                    case "2":
+                        Console.WriteLine("CIxarmaq istediyiniz meblegi yazin");
+                        decimal withdrawAmount = Convert.ToDecimal(Console.ReadLine());
+                        
+                        bankAccount1.WithDraw(withdrawAmount);
+                        break;
+                    default:
+                        Console.WriteLine("Sehv secim etdin,yeniden sec");
+                        break;
+                }
+            }
+            
+            }
+        }
+    }
+}
